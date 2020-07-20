7646a265a5260dd0db75b599324650037e1e24b7
Llausa <santiago.chamon@gmail.com>
2020-07-20 14:15:02 +1000

    FIX home_address and address

--- lib/xeroizer/models/payroll/address.rb
+++ lib/xeroizer/models/payroll/address.rb
@@ -14,34 +14,6 @@ module Xeroizer
         string      :city
         string      :country
         string      :postCode
-        class_inheritable_attributes :api_controller_name
-        class_inheritable_attributes :permissions
-        class_inheritable_attributes :xml_root_name
-        class_inheritable_attributes :optional_xml_root_name
-        class_inheritable_attributes :xml_node_name
-
-      end
-
-      class HomeAddressModel < AddressModel
-        set_xml_node_name 'HomeAddress'
-      end
-
-      class HomeAddress < Address
-        string      :address_line1
-        string      :address_line2
-        string      :address_line3
-        string      :address_line4
-        string      :city
-        string      :region
-        string      :postal_code
-        string      :country
-      end
-
-      class MailingAddressModel < AddressModel
-        set_xml_node_name 'MailingAddress'
-      end
-
-      class MailingAddress < Address
       end
 
     end
