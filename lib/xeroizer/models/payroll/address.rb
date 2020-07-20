7e7eac7cb3de1fb1fb7ac64624c891b3e2a4f3a8
Llausa <santiago.chamon@gmail.com>
2020-07-20 14:08:06 +1000

    FIX (trying to at least) HomeAddressModel

--- lib/xeroizer/models/payroll/address.rb
+++ lib/xeroizer/models/payroll/address.rb
@@ -27,6 +27,14 @@ module Xeroizer
       end
 
       class HomeAddress < Address
+        string      :address_line1
+        string      :address_line2
+        string      :address_line3
+        string      :address_line4
+        string      :city
+        string      :region
+        string      :postal_code
+        string      :country
       end
 
       class MailingAddressModel < AddressModel
